1e89ca10ece367f60000a663bb13c6be35af089f
Your Name <you@example.com>
2021-09-25 11:40:57 +0800

    update

--- jd_pet.js
+++ jd_pet.js
@@ -16,10 +16,10 @@
 
 =================================Loon===================================
 [Script]
-cron "15 6-18/6 * * *" script-path=jd_pet.js,tag=东东萌宠
+cron "15 1,6-18/6 * * *" script-path=jd_pet.js,tag=东东萌宠
 
 ===================================Surge================================
-东东萌宠 = type=cron,cronexp="15 6-18/6 * * *",wake-system=1,timeout=3600,script-path=jd_pet.js
+东东萌宠 = type=cron,cronexp="15 1,6-18/6 * * *",wake-system=1,timeout=3600,script-path=jd_pet.js
 
 ====================================小火箭=============================
 东东萌宠 = type=cron,script-path=jd_pet.js, cronexpr="15 6-18/6 * * *", timeout=3600, enable=true
